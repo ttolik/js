d7e4ec171b14f57c55bb274fdc0d0df25285108b
Anatolii Hordiienko <saske2013@ukr.net>
2017-05-09 16:45:17 +0300

    Added new files

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+var Name = document.getElementById("name");
+Name.innerText = prompt("What is your name ?");
+
+
+var age = document.getElementById("age");
+age.innerText = prompt("How old are you ?");
+
+
+var country = document.getElementById("country");
+country.innerText = prompt("Where are you from ?");
+
+
+var number = document.getElementById("number");
+number.innerText = prompt("What is your phone number ?");
+
+var address = document.getElementById("address");
+address.innerText = prompt ("What is your address ?");
+
+
